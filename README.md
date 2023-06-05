2ff6c04a5fbcc8176537f576c2144f826e1542c0
Luciano Palharez <luciano.palhares.ext@infracommerce.com.br>
2023-06-05 20:13:10 -0300

    projeto flutter novo

---
+++ README.md
@@ -0,0 +1,11 @@
+# camap_bd_flutter
+
+Usando recursos nativos do flutter
+
+## Camera
+
+## Mapas
+
+## Banco de Dados
+
+## etc
---
+++ android/app/src/main/kotlin/com/example/camap_bd_flutter/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.camap_bd_flutter
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
