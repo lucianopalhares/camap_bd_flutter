38ef90d8840d9e01dba66193d6ff52e08483e832
Luciano Palharez <luciano.palhares.ext@infracommerce.com.br>
2023-06-07 09:11:55 -0300

    ChangeNotifierProvider no main para a aplicacao escutar os NotifyListeners enviados por GreatPlaces

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,9 @@
+import 'package:camap_bd_flutter/providers/great_places.dart';
 import 'package:camap_bd_flutter/screens/place_form_screen.dart';
 import 'package:camap_bd_flutter/screens/places_list_screen.dart';
 import 'package:camap_bd_flutter/utils/app_routes.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 void main() {
   runApp(const MyApp());
@@ -13,17 +15,20 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Grate Places',
-      theme: ThemeData(
-        primarySwatch: Colors.indigo, 
-        accentColor: Colors.amber, 
-        visualDensity: VisualDensity.adaptivePlatformDensity
+    return ChangeNotifierProvider(
+      create: (ctx) => GreatPlaces(),
+      child: MaterialApp(
+        title: 'Grate Places',
+        theme: ThemeData(
+          primarySwatch: Colors.indigo, 
+          accentColor: Colors.amber, 
+          visualDensity: VisualDensity.adaptivePlatformDensity
+        ),
+        home: PlacesListScreen(), 
+        routes: {
+          AppRoutes.PLACE_FORM: (ctx) => PlaceFormScreen()
+        },
       ),
-      home: PlacesListScreen(), 
-      routes: {
-        AppRoutes.PLACE_FORM: (ctx) => PlaceFormScreen()
-      },
     );
   }
 }
